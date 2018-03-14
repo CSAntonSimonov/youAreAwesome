e73106aad93258fc53f5120342e90a5bf8002c3f
KoTuKJuBoTuK <simonovantond@gmail.com>
2018-03-14 18:54:17 +0300

    2, 3, 4

--- src/index.js
+++ src/index.js
@@ -1,18 +1,64 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
+const createEnumerableProperty = (prop) => {
+    return prop;
+};
+
+
+const createNotEnumerableProperty = (prop) => {
+    return Symbol(prop);
+};
+
+
+const createProtoMagicObject = () => {
+    function eat(){
+
+    }
+
+    eat.__proto__ = eat.prototype;
+
+    return eat;
+};
+
+let incrementorVal = 0;
+const incrementor = () => {
+  incrementorVal++;
+  if (incrementorVal === 3 || incrementorVal === 8 || incrementorVal === 10 || incrementorVal === 15) {
+    return incrementorVal;
+  }
+  return incrementor;
+};
+
+
 const asyncIncrementor = () => {};
+
+
 const createIncrementer = () => {};
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
+const returnBackInSecond = (param) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(param);
+      }, 1000);
+    })
+  };
+
+
 const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
-const toBuffer = () => {};
-const sortByProto = () => {};
+
+
+const createSerializedObject = () => {
+    return new String("Hello world");
+};
+
+
+const toBuffer = () => {
+    console.log("Log");
+};
+const sortByProto = (array) => {
+    return array.sort((a, b) => a - b);
+};
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
